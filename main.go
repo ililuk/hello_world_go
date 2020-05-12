0fc48954f3fced56b3bf280eae1327f44c6b24cf
Illia Lukashuk <ilu101207@gmail.com>
2020-05-12 09:00:59 +0300

    I added counter = counter + 2 three times

--- main.go
+++ main.go
@@ -4,7 +4,18 @@ import (
 	"fmt"
 )
 
-
-func main(){
+func main() {
 	fmt.Println("Hello world.my name is Illia")
+	fmt.Println("123")
+	var illia string
+	illia = "hello"
+	illia = "hello" + "world"
+	illia = illia + "world"
+	fmt.Println(illia)
+	var counter int
+	counter = counter + 2
+	counter = counter + 2
+	counter = counter + 2
+	fmt.Println(counter)
+
 }
